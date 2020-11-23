2f903f0eafb0d4cf9a579970a4480f31f2e50ec1
killvung <killvung@gmail.com>
2020-11-23 16:29:53 -0600

    type created

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import * as React from "react";
 
-const App = () => (
+const PLACEHOLDER: string = "Draw something on the board";
+
+interface AppProps {
+  result: Array<object>
+}
+
+const App: React.FC<AppProps> = (props) => (
   <div>
     <h1>Label</h1>
-    <h2>In Construction</h2>
+    <h2>{props.result.length === 0 ? PLACEHOLDER : "Report: "}</h2>
   </div>
 );
 
